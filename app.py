1ffde84482858e997a78c26f667c65206deda481
Joseph Shadrick <Joseph.Shadrick@mavs.uta.edu>
2023-11-20 09:54:57 -0600

    Added buttons to add/remove employees

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import requests
 from flask import Flask
+from flask import request
 from flask import render_template
 
 app = Flask(__name__)
+headers = {"Content-Type": "application/json"}
 
 @app.route("/")
 def home():
@@ -10,8 +12,24 @@ def home():
 
 @app.route("/employees")
 def get_employees():
-    data = requests.get("https://joseph-api-4ro5n.ondigitalocean.app/employees").json()
-    return render_template('employees.html', employees=data["employees"])
+    return render_template('employees.html', employees=requests.get(
+        #"https://joseph-api-4ro5n.ondigitalocean.app/employees"
+        "http://localhost:8080/employees"
+        ).json()["employees"])
+
+@app.route("/employees", methods=["POST"])
+def change_employees():
+    if request.form["add"]:
+        requests.post(
+            #"https://joseph-api-4ro5n.ondigitalocean.app/employees"
+            "http://localhost:8080/employees"
+            ,headers=headers,json={"name": request.form["add"]})
+    else:
+        requests.delete(
+            #"https://joseph-api-4ro5n.ondigitalocean.app/employees/"+request.form["text"]
+            "http://localhost:8080/employees/"+request.form["delete"]
+            )
+    return get_employees()
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
